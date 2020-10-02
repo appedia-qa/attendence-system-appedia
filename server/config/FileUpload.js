47c4fa5511142a8c655b30c38faa0dfd0e9c2f07
Arslan Sarwar <masarwar171@gmail.com>
2020-10-02 14:16:29 +0500

    file upload

--- server/config/FileUpload.js
+++ server/config/FileUpload.js
@@ -9,6 +9,14 @@ var storage = multer.diskStorage({
     }
 });
 
-const upload = multer({ storage: storage });
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+}
+
+const upload = multer({ storage: storage, fileFilter: fileFilter  });
 
 module.exports = upload;
