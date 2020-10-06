99a6a49c9bb341ec63cc78fb1b5199c22a0fdd5f
Arslan Sarwar <masarwar171@gmail.com>
2020-10-06 12:56:06 +0500

    redirect to admin dashboard with search word

--- client/src/constants/urls.js
+++ client/src/constants/urls.js
@@ -1,5 +1,6 @@
 export const baseUrl = `https://alhafeez.qa/api/users`;
 export const apiUrl = `https://alhafeez.qa/api`
+// export const apiUrl = `http://localhost:5000/api`
 export const googleApiKey =`AIzaSyCkUOsUDTcIiwzSIj29kX8ZQEYb7AoXfO8`
 export const authEndpoint = `/login`;
 export const signup = '/register';
