7b1df30620e3e069574db879a4bc881fa07e2a09
wmfarooqi05 <wmfarooqi05@gmail.com>
2020-10-09 14:10:45 +0500

    Update urls.js

--- client/src/constants/urls.js
+++ client/src/constants/urls.js
@@ -1,14 +1,14 @@
 
-export const baseUrl = `http://192.168.100.30:5000/api/users`;
-export const apiBaseUrl = `http://192.168.100.30:5000`
-export const apiUrl = `http://192.168.100.30:5000/api`
+// export const baseUrl = `http://192.168.100.30:5000/api/users`;
+// export const apiBaseUrl = `http://192.168.100.30:5000`
+// export const apiUrl = `http://192.168.100.30:5000/api`
+
+//Production
+export const baseUrl = `https://alhafeez.qa/api/users`;
+export const apiBaseUrl = `https://alhafeez.qa`
+export const apiUrl = `https://alhafeez.qa/api`
 
 
-// export const baseUrl = `https://alhafeez.qa/api/users`;
-// export const apiBaseUrl = `https://alhafeez.qa`
-// export const apiUrl = `https://alhafeez.qa/api`
-// export const apiUrl = `https://alhafeez.qa/api`
-// export const apiUrl = `http://localhost:5000/api`
 export const googleApiKey =`AIzaSyCkUOsUDTcIiwzSIj29kX8ZQEYb7AoXfO8`
 export const authEndpoint = `/login`;
 export const signup = '/register';
